432c8fa0a87f33dd74331952f556c8f53086f394
Anton Winogradov <winogradovaa@gmail.com>
2014-09-25 14:01:27 +0400

    Update structure and libs versions

--- desktop.examples/index/index.js
+++
@@ -1,7 +0,0 @@
-/*borschik:include:../../libs/bem-core/common.blocks/i-bem/__dom/i-bem__dom.js*/;
-/*borschik:include:../../libs/bem-core/common.blocks/jquery/jquery.js*/;
-/*borschik:include:../../libs/bem-core/common.blocks/loader/_type/loader_type_js.js*/;
-/*borschik:include:../../libs/bem-core/common.blocks/jquery/__config/jquery__config.js*/;
-/*borschik:include:../../libs/bem-core/common.blocks/dom/dom.js*/;
-/*borschik:include:../../libs/bem-core/common.blocks/i-bem/__dom/_init/i-bem__dom_init.js*/;
-/*borschik:include:../../libs/bem-core/common.blocks/i-bem/__dom/_init/i-bem__dom_init_auto.js*/;
